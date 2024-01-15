4373baf6b3f5d0ae71cb69d66030792be32560fb
Seunghyeok Lee <seunghyeok.tech@gmail.com>
2024-01-16 07:16:17 +0900

    Prettier를 사용한 자동 클래스 정렬

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button class="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+        Click me!
+      </button>
+    </>
+  );
 }
 
 export default App;
